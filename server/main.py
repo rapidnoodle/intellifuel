9f302d1652c1850097d3d72813aba2f5c5c6a1c6
mjlarson2 <mjlarson2@cps.edu>
2024-01-29 10:33:53 -0600

    parsed json for news components

--- server/main.py
+++ server/main.py
@@ -1,9 +1,18 @@
 import requests
 key = "073d144a53594de0b5f63a9aaa0fd8f1"
 url = ('https://newsapi.org/v2/top-headlines?'
-       'q= War &'
-       'sortBy=popularity&'
+       'q=war&'
+       'sortBy=relevancy&'
+       'language=en&'
        f'apiKey={key}')
 
 response = requests.get(url)
-print(response.json())
+headlines = []
+description = []
+content = []
+articlesDict = response.json()
+for i in range(0, len(articlesDict["articles"])):
+       headlines.append(articlesDict["articles"][i]["title"])
+       description.append(articlesDict["articles"][i]["description"])
+       content.append(articlesDict["articles"][i]["content"])
+print(headlines)
