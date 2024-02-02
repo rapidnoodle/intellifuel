5010de286db649a088ff6ab7929830f791c32b94
mjlarson2 <mjlarson2@cps.edu>
2024-02-02 10:25:31 -0600

    results limited by recency and oil mention

--- server/database.py
+++ server/database.py
@@ -1,18 +1,27 @@
 import requests
-key = "073d144a53594de0b5f63a9aaa0fd8f1"
-url = ('https://newsapi.org/v2/everything?'
-       'q=war&'
-       'sortBy=relevancy&'
-       'language=en&'
-       f'apiKey={key}')
+import time
 
-response = requests.get(url)
+key = "073d144a53594de0b5f63a9aaa0fd8f1"
 headlines = []
 description = []
 content = []
-articlesDict = response.json()
-for i in range(0, len(articlesDict["articles"])):
-       headlines.append(articlesDict["articles"][i]["title"])
-       description.append(articlesDict["articles"][i]["description"])
-       content.append(articlesDict["articles"][i]["content"])
-print(headlines)
+
+def retrieve():
+       currTime = time.strftime("%Y-%m-%d", time.gmtime( (time.time() - 86400) ) )
+       url = ('https://newsapi.org/v2/everything?'
+              'q=war%20AND%20oil%20NOT%20film%20NOT%20movie%20NOT%20gender%20NOT%20culture%20NOT%20plagiarism%20NOT%20"World War"%20NOT%20tech%20NOT%20legal%20NOT%20filmmaker&'
+              'sortBy=relevancy&'
+              'language=en&'
+              f'from={currTime}&'
+              'excludeDomains=engadget.com,techcrunch.com,arstechnica.com&'
+              f'apiKey={key}')
+
+       response = requests.get(url)
+       articlesDict = response.json()
+       for i in range(0, len(articlesDict["articles"])):
+              headlines.append(articlesDict["articles"][i]["title"])
+              description.append(articlesDict["articles"][i]["description"])
+              content.append(articlesDict["articles"][i]["content"])
+       
+
+retrieve()
